bc9ff7dd2e063ab1da9ac0a231670e5a26869905
ThenatDavid <86005086+ThenatDavid@users.noreply.github.com>
2021-06-16 16:05:50 +0200

    Add files via upload

---
+++ kosk.bat
@@ -0,0 +1,128 @@
+echo off
+
+echo THIS IS ONLY FUN PROJECT :D
+echo WAIT OR SKIP WTIH SPACE
+
+timeout 5
+
+echo FRIEND: HELLO, I HAVE ONE QUESTION
+
+timeout 30
+
+echo FRIEND: ARE YOU REALLY GAY?
+
+timeout 30
+
+echo YOU: NO IM NOT GAY BUT MY CLASSMATE IS GAY
+
+timeout 30
+
+echo FRIEND: RLY?
+
+timeout 30
+
+echo YOU: YES :D
+
+timeout 30
+
+echo FRIEND: WHO?
+
+timeout 30
+
+echo YOU: HE IS NEXT TO YOU :D
+
+timeout 5
+
+echo
+echo
+echo
+
+echo THIS IS FOR YOU
+
+echo
+echo
+echo
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start color 5
+start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start 
+
+
+@pause
